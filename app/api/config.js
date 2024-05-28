2d7a69b8eb28e709ff41a178abc6ef0d327dbdcf
Vladimir-Gore-razrab <maruninvovock2007@yandex.ru>
2024-05-28 21:29:26 +0300

    front

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://pindie-ot-gore-razraba.nomorepartiesco.ru/api";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+}
